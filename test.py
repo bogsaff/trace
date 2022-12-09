fe3d5f4ad9fabd1b5b84b2ffffb43e93b314d89b
bogsaff <bogsaff@gmail.com>
2022-12-09 22:28:53 +0300

    add smal graph

--- test.py
+++ test.py
@@ -1,2 +1,10 @@
-print("hi")
-print('yred')
+import networkx as nx
+
+
+new_Graph = nx.Graph()
+
+new_Graph.add_node(1)
+new_Graph.add_nodes_from([2, 3, 4, 5])
+#new_Graph.add_edges_from([2, 3])
+new_Graph.add_node(())
+print(new_Graph)
